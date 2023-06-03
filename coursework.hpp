a455a183982abfe63b260c80110c67896e0de73f
daryavanina <114079693+daryavanina@users.noreply.github.com>
2023-06-03 18:58:45 +0300

    Update coursework.hpp

--- coursework.hpp
+++ coursework.hpp
@@ -1,8 +1,12 @@
-﻿#pragma once
+#pragma once
 
 #include<iostream>
 #include<functional>
+#include<map>
 
+/**
+* @brief Класс для вычисления интеграла численным методом
+*/
 class Integral {
 public:
     Integral() = default;
@@ -11,19 +15,72 @@ public:
     Integral& operator=(const Integral& rhs) = default;
     ~Integral() = default;
 
+
 public:
+    //! Начало отрезка интегрирования
     double start_{ 0 };
+    //! Конец отрезка интегрирования
     double finish_{ 0 };
+    //! Шаг, с которым высчитывается интеграл (количество прямоугольников на отрезке)
     int step_{ 1 };
+    
 };
 
+/**
+* @brief Функция для вычисления интеграла методом левых прямоугольников
+* @param f подынтегральная функция
+* @param a начало отрезка интегрирования
+* @param b конец отрезка интегрирования
+* @param n количество прямоугольников в отрезке
+* @return численное значения интеграла
+*/
 double Left_Rect(std::function<double(double)> f, double a, double b, double n);
+
+/**
+* @brief Функция для вычисления интеграла методом правых прямоугольников
+* @param f подынтегральная функция
+* @param a начало отрезка интегрирования
+* @param b конец отрезка интегрирования
+* @param n количество прямоугольников в отрезке
+* @return численное значения интеграла
+*/
 double Right_Rect(std::function<double(double)> f, double a, double b, double n);
+
+/**
+* @brief Функция для вычисления интеграла методом трапеций
+* @param f подынтегральная функция
+* @param a начало отрезка интегрирования
+* @param b конец отрезка интегрирования
+* @param n количество трапеций в отрезке
+* @return численное значения интеграла
+*/
 double Trapezoid(std::function<double(double)> f, double a, double b, double n);
+
+/**
+* @brief Функция для вычисления интеграла методом Симпсона
+* @param f подынтегральная функция
+* @param a начало отрезка интегрирования
+* @param b конец отрезка интегрирования
+* @param n количество криволинейных трапеций в отрезке
+* @return численное значения интеграла
+*/
 double Simpson(std::function<double(double)> f, double a, double b, double n);
 
-double MAINF(std::function<double(double)> f,
-    std::function<double(std::function<double(double)>, double, double, double)> g,
+/**
+* @brief функция для вывода численного ответа
+* @param метод для вычисления интеграла
+* @param подынтегральная функция
+* @param вычисляемый интеграл
+* @return
+*/
+double MAINF(std::function<double(std::function<double(double)>, double, double, double)> method,
+    std::function<double(double)> function,
     const Integral& rhs);
 
-void Out_to_Tex(const Integral& rhs, const std::string fun, const int meth);
+/**
+* @brief функция для вывода в Tex
+* @param вычисляемый интеграл
+* @param подынтегральная функция
+* @param метод, которым вычисляется интеграл
+*/
+void Out_to_Tex(const Integral& rhs, const std::string fun, const std::string meth);
