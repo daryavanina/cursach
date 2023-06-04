9f3e0671dd7c24fa7bbe8e794f4c7f1a0eb30d59
daryavanina <114079693+daryavanina@users.noreply.github.com>
2023-06-04 13:44:18 +0300

    Update console.cpp

--- console.cpp
+++ console.cpp
@@ -1,4 +1,5 @@
 #include"coursework.hpp"
+#define _USE_MATH_DEFINES
 #include<iostream>
 #include<functional>
 #include<sstream>
@@ -10,49 +11,47 @@ int main(
     char* argv[])  // Array of command-line argument strings
 {
     std::cout.precision(7);
-    double start = 1;
-    double finish = 2;
-    double step = 10;
-    std::string fun = "";//function in map_fun
-    std::string meth = "";//method number in map_meth
-    start = std::stod(argv[1]);
-    finish = std::stod(argv[2]);
-    step = std::stod(argv[3]);
-    fun += argv[4];
-    meth += argv[5];
-    Integral check = Integral(start, finish, step);
-
-    std::map < std::string, std::function<double(double)>> map_fun{//map of functions
-        {"sin",
-                [](double x) {
-                    return sin(x);
-                }
-        },
-        {"cos",
-                [](double x) {
-                    return cos(x);
-                }
-        },
-        {"sqx",
-                [](double x) {
-                    return x*x;
-                }
-        }
-    };
-    //std::map < std::string, std::function<double(std::function<double(double)>, double, double, int)>> map_meth{//method map
-    //    {"1", &Integral::Left_Rect},
-    //    {"2", &Integral::Right_Rect},
-    //    {"3", &Integral::Trapezoid},
-    //    {"4", &Integral::Simpson}
-    //};
-
-    if (!(map_fun.find(fun) == map_fun.end()) && check.Method(meth)==0 ||//map_meth.find(meth) == map_meth.end() ||
-         (step<=0) || (finish<start)) {//abequacy check
-        std::cout << "Incorrent data";
+    if (argc != 6) {
+        std::cout<< "Incorrent data";
     }
     else {
-        std::cout << check.MAINF(check.Method(meth), map_fun[fun], check);
-        check.Out_to_Tex(check, fun, meth);
+        double start = 1;
+        double finish = 2;
+        double step = 10;
+        std::string fun = "";//function in map_fun
+        std::string meth = "";//method number in map_meth
+        start = std::stod(argv[1]);
+        finish = std::stod(argv[2]);
+        step = std::stod(argv[3]);
+        fun += argv[4];
+        meth += argv[5];
+        Integral check = Integral(start, finish, step);
+
+        std::map < std::string, std::function<double(double)>> map_fun{//map of functions
+            {"sin",
+                    [](double x) {
+                        return sin(x);
+                    }
+            },
+            {"cos",
+                    [](double x) {
+                        return cos(x);
+                    }
+            },
+            {"sqx",
+                    [](double x) {
+                        return x * x;
+                    }
+            }
+        };
+        if (map_fun.find(fun) == map_fun.end() || check.Method(meth) == 0 ||
+            (step <= 0) || (finish < start)) {//abequacy check
+            std::cout << "Incorrent data";
+        }
+        else {
+            std::cout << check.MAINF(check.Method(meth), map_fun[fun], check);
+            check.Out_to_Tex(check, fun, meth);
+        }
+        return 0;
     }
-    return 0;
 }
